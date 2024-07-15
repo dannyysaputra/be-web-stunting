21f44cc156cbe7f144e2849db27c22bc18713768
Danny Suggi Saputra <dannysaputra3003@gmail.com>
2024-07-15 12:29:34 +0700

    login and create token

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -1,18 +1,10 @@
 import { Router } from "express";
 import { AuthController } from "../app/controller/api/v1/AuthController";
 import upload from "../app/middlewares/multer";
-import swaggerui from 'swagger-ui-express';
-import swaggerDocument from '../openapi.json'; 
 
 const router = Router();
 
 router.post("/register", upload.single('image'), AuthController.register);
-
-router.use('/api-docs', swaggerui.serve);
-router.get('/api-docs', swaggerui.setup(swaggerDocument));
-router.get('/api-docs.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerDocument);
-})
+router.post("/login", AuthController.login);
 
 export default router;
