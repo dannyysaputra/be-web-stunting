ec9ca37bb1cb688acaa1f6b8669bd5df30f4b9f1
dannyysaputra <84920467+dannyysaputra@users.noreply.github.com>
2024-08-13 20:01:47 +0700

    fix: avatar users nullable

--- app/models/UserModel.ts
+++ app/models/UserModel.ts
@@ -6,7 +6,7 @@ export type UserType = {
     email: string;
     handphone_number: string;
     password: string | null;
-    avatar: string;
+    avatar: string | null;
     role_id: number;
     created_at: Date;
     updated_at: Date;
